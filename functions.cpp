7302dee84e219df4eadae2bc307a9e50904e440f
NaiPret <ngvphu235e@gmail.com>
2022-11-13 18:23:52 +0700

    update func

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
 
 using namespace std;
+
+int sum(int a, int b = 0, int c = 0, int d = 0, int e = 0, int f = 0, int g = 0, int h = 0, int i = 0, int j = 0) {
+    return a + b + c + d + e + f + g + h + i + j;
+}
+
+int sum_array(int num, int arr[]) {
+    int sum {};
+    for (int i = 0; i < num; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+double sum_array(int num, double arr[]) {
+    double sum {};
+    for (int i = 0; i < num; i++) {
+        sum += 1.0 * arr[i];
+    }
+    return sum;
+}
+
+long int factorial(int num) {
+    long int factorial = 1;
+    if (num < 0)
+        return -1;
+    if (num == 0 || num == 1)
+        return 1;
+    for(int i = 1; i <= num; ++i) {
+        factorial *= (long)i;
+    }
+    return factorial;
+}
--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
-    cout << "My name is: NaiPret";
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+    cout << sum_array(5, arr) << endl;
+    cout << sum(5, 1, 2, 3, 4) << endl;
+    cout << factorial(5) << endl;
+    int a = 5;
+    int b = 10;
+    swap(a, b);
+    cout << a << " " << b;
     return 0;
 }
